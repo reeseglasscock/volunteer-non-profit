b958f0373c7738c379dcf94acbec5a06214037f4
reeseglasscock <reesegpdx@gmail.com>
2018-07-13 16:10:50 -0700

    get route to project page success

--- app.rb
+++ app.rb
@@ -18,6 +18,12 @@ post('/') do
   new_project = Project.new({:id => nil, :title => title})
   new_project.save
   @projects = Project.all
-  binding.pry
   erb(:index)
+end
+
+get('/project/:id') do
+  id = params[:id].to_i
+  @project = Project.find(id)
+  @projects = Project.all
+  erb(:manage_project)
 end
